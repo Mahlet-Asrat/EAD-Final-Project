b41bcc4a7d29a33772652908c8ab590f4233a5b7
Niyat Debesay <niyatdebesay26@gmail.com>
2025-01-31 09:31:43 +0300

    Fix warnings on  AppointementService module

--- src/main/java/com/example/Security/Salon/Appointment/Controller/AppointmentController.java
+++ src/main/java/com/example/Security/Salon/Appointment/Controller/AppointmentController.java
@@ -5,7 +5,7 @@ import com.example.Security.Salon.Appointment.Model.Dto.AppointmentResponseDto;
 import com.example.Security.Salon.Appointment.Model.Dto.CreateAppointmentDTO;
 import com.example.Security.Salon.Appointment.Service.AppointmentService;
 import com.example.Security.Salon.Exception.ResourceNotFoundException;
-import com.example.Security.Salon.User.Model.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
--- src/main/java/com/example/Security/Salon/Appointment/Model/Dto/AppointmentResponseDto.java
+++ src/main/java/com/example/Security/Salon/Appointment/Model/Dto/AppointmentResponseDto.java
@@ -2,13 +2,18 @@ package com.example.Security.Salon.Appointment.Model.Dto;
 
 import com.example.Security.Salon.Appointment.Model.AppointmentService;
 import com.example.Security.Salon.Appointment.Model.Status;
-import com.example.Security.Salon.Service.Model.Service;
 import com.example.Security.Salon.User.Model.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+@Data
+
+@NoArgsConstructor
 public class AppointmentResponseDto {
 
     private UUID appointmentId;
